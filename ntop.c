7124e2f06ad028e21c2f8a8e9f9b67b0f0bf85c0
yd3v <ythalyson@gmail.com>
2021-04-18 06:53:18 -0300

    update

--- ntop.c
+++ ntop.c
@@ -3,11 +3,15 @@
 #include <stdio.h>
 int main(int argc, char* argv[]) {
 	if(argc < 2) return 1;
-	char buff[20];
+
+	char buff[16];
 	struct hostent *host;
+
 	host = gethostbyname(argv[1]);
+
 	if(!host) return 1;
 	inet_ntop(AF_INET, host->h_addr, buff, sizeof buff);
+
 	printf("%s\n", buff);
 	return 0;
 }
