9315719c36251613ce8491db0daf2c0085e506e4
yd3v <ythalyson@gmail.com>
2021-04-18 13:12:14 -0300

    update

--- gethostname.c
+++ gethostname.c
@@ -1,9 +1,16 @@
 #include <unistd.h> // gethostname
+#include <arpa/inet.h> //inet_ntop
+#include <netdb.h> //gethostbyname, hostent
 #include <stdio.h>
 #include <malloc.h>
 int main() {
-	char *hostname = (char*) malloc(30);
+	char *hostname = (char*)malloc(30);
+	struct hostent *address;
+	char *ip_addr = (char*)malloc(30);
 	gethostname(hostname, sizeof hostname); // (char* name, size_t len)
-	printf("hostname -> %s\n", hostname);
+	address = gethostbyname(hostname);
+
+	inet_ntop(AF_INET, address->h_addr, ip_addr, sizeof ip_addr);
+	printf("hostname -> %s\naddress -> '%s'\n", hostname, ip_addr);
 	return 0;
 }
