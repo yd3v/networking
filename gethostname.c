daddfed3c6bf05ea1769ce871f046677a9f4c16d
yd3v <ythalyson@gmail.com>
2021-04-18 07:38:14 -0300

    gethostname

--- gethostname.c
+++ gethostname.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <malloc.h>
 int main() {
-	char *hostname = (char*) malloc(30 * sizeof(char*));
+	char *hostname = (char*) malloc(30);
 	gethostname(hostname, sizeof hostname); // (char* name, size_t len)
 	printf("hostname -> %s\n", hostname);
 	return 0;
